790d503f9504b4f740df6a0b67defd91f2bf0f37
Chandra Mouli Kantamaneni <72177678+kcmchandramouli@users.noreply.github.com>
2026-03-05 21:48:49 +0530

    Create conanfile.py for Conan2 Dummy Project
    Add Conanfile for Conan2 Dummy Project to test toolchains.

---
+++ packages/conan2_dummy/conanfile.py
@@ -0,0 +1,38 @@
+"""Conanfile for Conan2 Dummy Project to test the different toolchains."""
+
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
+
+
+class Conan2Dummy(ConanFile):
+    """Conan2 Dummy Project to test the different toolchains."""
+
+    name = "conan2_dummy"
+    settings = "os", "compiler", "arch", "build_type"
+    license = "ZF"
+    url = (
+        "**************"
+    )
+    description = "Conan dummy repository"
+
+    def generate(self):
+        """Run the generators for the project."""
+        deps = CMakeDeps(self)
+        deps.generate()
+
+        toolchain = CMakeToolchain(self, "Ninja")
+        toolchain.variables["USE_CPLUSPLUS_FILES"] = (
+            str(self.settings.compiler) == "gcc"  # pylint: disable=no-member
+        )
+        toolchain.generate()
+
+    def build(self):
+        """Build the project using CMake."""
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        """Package the project using CMake."""
+        cmake = CMake(self)
+        cmake.install()
